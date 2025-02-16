1cfd6cf1d1b049573a4f00165beb01616faa8dd3
Alex S <justam4444@gmail.com>
2025-02-16 23:19:34 +0300

    UPDATE: now the module also removes line breaks

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
-  };
+    return string.replace(/\s+/g, "");
+};
+module.exports = function tiny(string) {
+  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+  return string.replace(/\s+/g, "");
+};
